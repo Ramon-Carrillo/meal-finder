12181f568b2f0d83f0d72a71f125601adb5ab2bb
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-05-06 19:19:43 -0500

    finished js and the project

--- script.js
+++ script.js
@@ -49,5 +49,87 @@ const searchMeal = async (e) => {
   }
 }
 
+//* Fetch meal by ID
+const getMealById = async (mealID) => {
+  try {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`,
+    )
+    const data = await res.json()
+    const meal = data.meals[0]
+
+    addMealToDOM(meal)
+  } catch (error) {
+    console.log('No Good Dr. Jones')
+  }
+}
+
+//* Fetch random meal from API
+const randomMeal = async () => {
+  //*Clear meals and heading
+  mealsEl.innerHTML = ''
+  resultHeading.innerHTML = ''
+
+  try {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/random.php`,
+    )
+    const data = await res.json()
+    const meal = data.meals[0]
+
+    addMealToDOM(meal)
+  } catch (error) {
+    console.log('No Good Dr. Jones')
+  }
+}
+
+//* Add meal to DOM
+const addMealToDOM = (meal) => {
+  const ingredients = []
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`,
+      )
+    } else {
+      break
+    }
+  }
+
+  single_mealEl.innerHTML = `
+  <div class='single-meal'>
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map((ing) => `<li>${ing}</li>`).join('')}
+      </ul>
+    </div>
+  </div>
+  `
+}
+
 //* Event Listeners
 submit.addEventListener('submit', searchMeal)
+random.addEventListener('click', randomMeal)
+
+mealsEl.addEventListener('click', (e) => {
+  const mealInfo = e.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info')
+    } else {
+      return false
+    }
+  })
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid')
+    getMealById(mealID)
+  }
+})
